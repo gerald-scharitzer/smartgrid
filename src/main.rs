6c79de72c56e9810ee346ae0e7398b12c0dd1d2c
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-07-20 22:42:19 +0200

    node

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+struct Node {
+    power: f64, // current power in watt - Producers have positive power. Consumers have negative power.
+    power_max: f64, // maximum power in watt - the peak capacity of producers
+}
+
 fn main() {
+    let mut node = Node {
+        power: 1_000_000.0,
+        power_max: 10_000_000.0,
+    };
     println!("The Smart Grid");
+    println!("power: {} W", node.power);
 }
