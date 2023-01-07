9cf56a7835c107fa5f8a1c70e1a2873a3ee5ae13
gerald-scharitzer <gerald.scharitzer@gmail.com>
2023-01-07 19:53:51 +0100

    source and sink

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use smartgrid::Node;
 
 fn main() {
-    let mut node = Node::new(1_000_000.0, 10_000_000.0, 0.0);
-    node.set_power(2_000_000.0);
+    let mut source = Node::new(1_000_000.0, 10_000_000.0, 0.0);
+    source.set_power(2_000_000.0);
+    let mut sink = Node::new(-1_000_000.0, 0.0, -5_000_000.0);
+    sink.set_power(-2_000_000.0);
     println!("The Smart Grid");
-    println!("power: {} W", node.get_power());
+    println!("source: {} W", source.get_power());
+    println!("sink: {} W", sink.get_power());
 }
